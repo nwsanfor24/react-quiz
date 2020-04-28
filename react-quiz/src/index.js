0f87721ab6c7e14a417cef70f13b03ac9bb57f07
Nathan Sanford <nwsanfor24@gmail.com>
2020-04-28 14:27:03 -0400

    made a root index

---
+++ react-quiz/src/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import ".assets/style.css";
+
+class Quiz extends Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="title">React Quiz</div>
+            </div>
+        );
+    }
+}

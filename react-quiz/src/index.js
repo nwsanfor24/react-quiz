c04b2a820335a92168b60cf244bf121adeb7cbdd
Nathan Sanford <nwsanfor24@gmail.com>
2020-04-28 16:00:14 -0400

    rendering answers when buttons are clicked

--- react-quiz/src/index.js
+++ react-quiz/src/index.js
@@ -2,10 +2,13 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import "./assets/style.css";
 import quizService from "./quizService";
+import QuestionBox from "./components/QuestionBox";
 
 class Quiz extends Component {
     state = {
-        questionBank: []
+        questionBank: [],
+        score: 0,
+        responses: 0
     };
 
     getQuestions = () => {
@@ -15,7 +18,16 @@ class Quiz extends Component {
             });
         });
     };
-
+    computerAnswer = (answer, correctAnswer) => {
+        if (answer === correctAnswer) {
+            this.setState({
+                score: this.state.score + 1
+            });
+        }
+        this.setState({
+            responses: this.state.responses < 5 ? this.state.responses + 1 : 5
+        });
+    }
     componentDidMount() {
         this.getQuestions();
     }
@@ -25,7 +37,15 @@ class Quiz extends Component {
             <div className="container">
                 <div className="title">React Quiz</div>
                 {this.state.questionBank.length > 0 && this.state.questionBank.map(({question, answers,
-                correct, questionId}) => (<h4>{question}</h4>))}
+                correct, questionId}) => (
+                    <QuestionBox 
+                        question={question} 
+                        options={answers} 
+                        key={questionId}
+                        selected={answer => this.computeAnswer(answer, correct)}
+                    />
+                    )
+                )}
             </div>
         );
     }

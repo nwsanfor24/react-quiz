ad8d7985cacbcdef0e3c57800345d821a40bee26
Nathan Sanford <nwsanfor24@gmail.com>
2020-04-28 14:38:24 -0400

    import quiz api

--- react-quiz/src/index.js
+++ react-quiz/src/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import "./assets/style.css";
+import quizService from "./quizService";
 
 class Quiz extends Component {
     render() {
